7211d55817307383ba6a35b17da7c22b7d7477e3
Vijay Mahantesh SM <vijaym123@gmail.com>
2014-03-07 14:30:56 -0600

    seperated static content with dynamic content

--- scrapeData.py
+++ scrapeData.py
@@ -8,7 +8,7 @@ import nltk
 import fractions
 from collections import defaultdict
 
-class Food:
+class FoodResources:
 	meatItems = ["Beef","Bison","Dog","Game","Bear","Venison","Wild Boar","Goat",\
 				"Horse","Lamb","Mutton","Pork","Rabbit","Turtle","Veal","Chicken", \
 				"Cornish Game Hen","Duck","Quail","Turkey","Ostrich","Goose","Bacon",\
@@ -21,7 +21,7 @@ class Food:
 	measurements = {}
 	types = ["Baked","Baking", "Barbecue","Braise", "Camping", "Fermented", "Fried", \
 				"Marinade", "Microwave", "Slow cooker", "Smoked", "Stir fry"]
-
+	
 	posTags = dict([("CC","Coordinating conjunction"),("CD","Cardinal number"),("DT","Determiner"),("EX","Existential there"),("FW","Foreign word"),
 			("IN","Preposition or subordinating conjunction"),("JJ","Adjective"),("JJR","Adjective, comparative"),("JJS","Adjective, superlative"),
 			("LS","List item marker"),("MD","Modal"),("NN","Noun, singular or mass"),("NNS","Noun, plural"),("NNP","Proper noun, singular"),
@@ -30,66 +30,12 @@ class Food:
 			("VB","Verb, base form"),("VBD","Verb, past tense"),("VBG","Verb, gerund or present participle"),
 			("VBN","Verb, past participle"),("VBP","Verb, non-rd person singular present"),("VBZ","Verb, rd person singular present"),("WDT","Wh-determiner"),
 			("WP","Wh-pronoun"),("WP$","Possessive wh-pronoun"),("WRB","Wh-adverb")])
-		
-	recipe = {}
-	def __init__(self, name, serves = 12):
+
+	def __init__(self):
 		self.getAllIngredients()
 		self.getAllTechniques() 
 		self.getAllEquipments() 
 		self.getAllMeasurements()
-		self.getRecipe(name, serves)
-
-	def getNutrients(self, soupBody):
-		nutrients = {}
-		for element in zip([i.find("li",{"class":"categories"}).text for i in soupBody.findAll("ul",{"id":"ulNutrient"})],\
-							   [i.find("li",{"class":"units"})for i in soupBody.findAll("ul",{"id":"ulNutrient"})]):
-			nutrients[element[0]] = (element[1].span.text, element[1].text[len(element[1].span.text):])
-		return nutrients
-
-	def labelIngredients(self, ingredientText, amountText):
-		"""
-		amount = number + measurement
-		ingredient = descriptor + preparation + item
-		"""
-		tokens = nltk.word_tokenize(ingredientText.replace(",",""))
-		tags = nltk.pos_tag(tokens)
-		label = defaultdict(list)
-		label["ingredient"] = ingredientText
-		for i in tags:
-			print i
-			if i[1].startswith("JJ"):
-				label["descriptor"].append(i[0])
-			elif i[1].startswith("RB") or i[1].startswith("VB"):
-				label["preparation"].append(i[0])
-			else:
-				label["item"].append(i[0])
-
-		label["descriptor"] = " ".join(label["descriptor"])
-		label["preparation"] = " ".join(label["preparation"])
-		label["item"] = " ".join(label["item"])
-		
-		tokens = nltk.word_tokenize(amountText.replace(",",""))
-		label["amount"] = amountText
-		label["number"] = 0
-		label["measurement"] = []
-		for i in tokens:
-			try:
-				label["number"] +=  float(fractions.Fraction(i))
-			except ValueError:
-				label["measurement"].append(i)
-		label["measurement"] = " ".join(label["measurement"])
-		return dict(label) 
-
-	def getIngredients(self, soupBody):
-		iterator = zip([i.text for i in soupBody.findAll("span",{"id":"lblIngName"})],[ i.text for i in soupBody.findAll("span",{"id":"lblIngAmount"})])
-		return [self.labelIngredients(item[0],item[1]) for item in iterator]
-
-	def meatAndPoultry(self, name):
-		"""
-		Source wikipedia : http://en.wikibooks.org/wiki/Cookbook:Meat_and_poultry
-		Modify this to work on any given item.
-		"""
-		return difflib.get_close_matches(name,self.meatItems)
 
 	def getAllIngredients(self):
 		title = "http://en.wikibooks.org/wiki/"
@@ -122,8 +68,67 @@ class Food:
 			self.measurements["weight"] = dict(test[11:17])
 			self.measurements["length"] = dict(test[17:21])
 
+class Food:
+	recipe = {}
+	resource = None
+
+	def __init__(self, name, serves = 12):
+		self.getRecipe(name, serves)
+		self.resource = FoodResources()
+
+	def getNutrients(self, soupBody):
+		nutrients = {}
+		for element in zip([i.find("li",{"class":"categories"}).text for i in soupBody.findAll("ul",{"id":"ulNutrient"})],\
+							   [i.find("li",{"class":"units"})for i in soupBody.findAll("ul",{"id":"ulNutrient"})]):
+			nutrients[element[0]] = (element[1].span.text, element[1].text[len(element[1].span.text):])
+		return nutrients
+
+	def labelIngredients(self, ingredientText, amountText):
+		"""
+		amount = number + measurement
+		ingredient = descriptor + preparation + item
+		"""
+		tokens = nltk.word_tokenize(ingredientText.replace(",",""))
+		tags = nltk.pos_tag(tokens)
+		label = defaultdict(list)
+		label["ingredient"] = ingredientText
+		for i in tags:
+			if i[1].startswith("JJ"):
+				label["descriptor"].append(i[0])
+			elif i[1].startswith("RB") or i[1].startswith("VB"):
+				label["preparation"].append(i[0])
+			else:
+				label["item"].append(i[0])
+
+		label["descriptor"] = " ".join(label["descriptor"])
+		label["preparation"] = " ".join(label["preparation"])
+		label["item"] = " ".join(label["item"])
+		
+		tokens = nltk.word_tokenize(amountText.replace(",",""))
+		label["amount"] = amountText
+		label["number"] = 0
+		label["measurement"] = []
+		for i in tokens:
+			try:
+				label["number"] +=  float(fractions.Fraction(i))
+			except ValueError:
+				label["measurement"].append(i)
+		label["measurement"] = " ".join(label["measurement"])
+		return dict(label) 
+
+	def getIngredients(self, soupBody):
+		iterator = zip([i.text for i in soupBody.findAll("span",{"id":"lblIngName"})],[ i.text for i in soupBody.findAll("span",{"id":"lblIngAmount"})])
+		return [self.labelIngredients(item[0],item[1]) for item in iterator]
+
+	def meatAndPoultry(self, name):
+		"""
+		Source wikipedia : http://en.wikibooks.org/wiki/Cookbook:Meat_and_poultry
+		Modify this to work on any given item.
+		"""
+		return difflib.get_close_matches(name, self.resource.meatItems)
+
 	def TypeOfPreparation(self, name):
-		return difflib.get_close_matches(name,self.types)
+		return difflib.get_close_matches(name, self.resource.types)
 		
 	def getRecipe(self, name, serves):
 		page = url.urlopen("http://allrecipes.com/Recipe/"+name+"?scale="+str(serves)+"&ismetric=0")
