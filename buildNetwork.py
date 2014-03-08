6c7bb866561a638812c414e9bdc50805215f12c6
Vijay Mahantesh SM <vijaym123@gmail.com>
2014-03-08 15:15:32 -0600

    data for 537 recipies collected and converted to gml format

--- buildNetwork.py
+++ buildNetwork.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 	data = json.loads(f.read())
 	f.close()
 	G = nx.Graph()
-	for item1,item2 in itertools.combinations(data.keys(),2):
+	for item1,item2 in itertools.combinations(data.keys()[:50],2):
 		if set(data[item1]['recipe']).intersection(set(data[item2]['recipe'])):
 			G.add_edge(item1,item2)
 	nx.write_gml(G,"breakfast-and-brunch.gml")
